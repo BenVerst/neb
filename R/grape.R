eeb56b7693251bde2584b954627d5667137d2495
BenVerst <Bjwversteeg@gmail.com>
2023-08-25 10:59:50 -0700

    Initial commit

---
+++ R/grape.R
@@ -0,0 +1,14 @@
+#' An eighties inspired ggplot theme
+#'
+#' @return
+#' @export
+#'
+#' @examples
+grape_juice <- function() {
+  theme(plot.background = element_rect(fill = "darkgreen"),
+        panel.background = element_rect(fill = "purple"),
+        axis.text = element_text(color = "lightgray"),
+        panel.grid = element_line(color = "black"))
+}
+#devtools::document
+#^
